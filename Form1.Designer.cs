8f28bd829fee6e472829617d640609249853d0b6
bz023 <biro.zoltan023@gmail.com>
2021-10-17 17:21:41 +0200

    ablak nagyitas/kicsinyites gomb jobb felulrol kiveve

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -331,7 +331,9 @@ namespace etteremProjekt
             this.Controls.Add(this.gbox_rendelo);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(1134, 605);
+            this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(1134, 605);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
